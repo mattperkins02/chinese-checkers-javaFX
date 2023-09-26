3a0db6d4cf2a6be9451b0e0e8898cd0725b3b8af
Matthew Perkins <tuk96178@temple.edu>
2023-09-26 14:56:29 -0400

    Trying stuff out
    I am trying to figure out how to make a working JavaFX project and connecting it to the repo

---
+++ demo/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.example.demo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.demo to javafx.fxml;
+    exports com.example.demo;
+}
